95b67ae3da4fad06dd6daa8bef012f46dc78a500
SEBASTIANHANBERG <SEBASTIAN.HANBERG@HOTMAIL.COM>
2022-11-15 17:12:24 +0100

    set up project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'pokemon-yellow': '#ffcb05',
+      'pokemon-blue': '#5db9ff',
+    }
+  },
+  plugins: [],
+}
